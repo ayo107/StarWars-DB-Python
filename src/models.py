28f22efaf761c99d0c288cb4e67306af332d3e81
ayo107 <ayo_linkin10@hotmail.com>
2022-06-08 22:12:18 +0000

    primera subida

--- src/models.py
+++ src/models.py
@@ -7,24 +7,59 @@ from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
+class Usuarios(Base):
+    __tablename__ = "usuarios"
+    ID = Column(Integer, primary_key=True)
+    Nombre = Column(String(250))
+    Apellidos = Column(String(250))
+    Direccion = Column(String(250))
+    Correo = Column(String(250))
+    Nombre_Usuario = Column(String(250))
+    FavoritoID = Column(Integer, ForeignKey('favoritos.FavoritoID'))
+    
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class Personajes(Base):
+    __tablename__ = 'Personajes'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    PersonajeID = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    Height = Column(Integer)
+    Mass = Column (Integer)
+    Color_Eye = Column (String(250), nullable=False)
+    FavoritoID = Column(Integer, ForeignKey('favoritos.FavoritoID'))
+    UsuarioID = Column(Integer, ForeignKey('usuarios.ID'))
 
-class Address(Base):
-    __tablename__ = 'address'
+class Planetas(Base):
+    __tablename__ = 'planetas'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    PlanetaID = Column(Integer, primary_key=True)
+    Name = Column(String(250))
+    Orbita = Column(String(250))
+    Population = Column(String(250), nullable=False)
+    Diameter = Column(Integer)
+    Gravity = Column(String(250))
+    FavoritoID = Column(Integer, ForeignKey('favoritos.FavoritoID'))
+    UsuarioID = Column(Integer, ForeignKey('usuarios.ID'))
+
+    class Naves(Base):
+        __tablename__ = 'naves'
+        NaveID = Column(Integer, primary_key=True)
+        Cost = Column(String(250))
+        Capacity = Column(String(250))
+        Speed = Column (String(250))
+        Mass = Column (String(250))
+        FavoritoID = Column(Integer, ForeignKey('favoritos.FavoritoID'))
+        UsuarioID = Column(Integer, ForeignKey('usuarios.ID'))
+        class Favoritos(Base):
+            __tablename__ = 'favoritos'
+            FavoritoID = Column(Integer, primary_key=True)
+            PersonajeID = Column(Integer, ForeignKey('Personajes.PersonajeID'))
+            NaveID = Column(Integer, ForeignKey('naves.NaveID'))
+            PersonajeID = Column(Integer, ForeignKey('planetas.PlanetaID'))
+            UsuarioID = Column(Integer, ForeignKey('usuarios.ID'))
 
     def to_dict(self):
         return {}
